e86d00082af96f279a7a3e3ed0a309bfc0535d24
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2023-10-03 20:00:52 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,52 @@
+        }
+            System.out.println("Random Number: " + item);
+    }
+        int[] data = new int[10];
+    public static void main(String[] args) {
+        for (int item : data) {
+        }
+
+public class RandomDataGenerator {
+}
+        for (int i = 0; i < 10; i++) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+import random
+    return data
+    for item in data:
+
+    data = generate_random_data()
+def main():
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()        print(f"Random Number: {item}")
+
+
+    return data
+if __name__ == "__main__":
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+def generate_random_data():
+import random
+
+def main():
+    main()def main():
+if __name__ == "__main__":
+    data = generate_random_data()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+    main()
+import random
