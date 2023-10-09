a7b9760e3d133404af87eef0eee3625b8693e4ee
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2023-10-09 02:04:51 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,26 @@
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
 
-        }
-}
-
-        }
-    public static void main(String[] args) {
-    }
-public class RandomDataGenerator {
         for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+        }
+
+}
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        for (int item : data) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;if __name__ == "__main__":
+    return data
+    main()
+    data = generate_random_data()
+
+
+
+        print(f"Random Number: {item}")
+def main():
+    for item in data:
+import random
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
