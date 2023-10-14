8f1722f539ab4a5b4f3ee880736773ee84438d29
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2023-10-14 07:57:15 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,13 +1,28 @@
-import random
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-def main():
-def generate_random_data():
+    }
+    return data;
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    });
+        console.log("Random Number: " + item);
 
-    main()
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
+    for (let i = 0; i < 10; i++) {
+main();
+}
+function generateRandomData() {
 
+    data.forEach(item => {
+    const data = [];
 
-    for item in data:
-    data = generate_random_data()
+    const data = generateRandomData();        int[] data = new int[10];
+        }
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+        }
+    public static void main(String[] args) {
+    }
+            System.out.println("Random Number: " + item);
+
+public class RandomDataGenerator {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
